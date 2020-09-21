e5b8b33adff604938cf846fb2c1ee6a578762d31
Lucas Aoun <souolucas2009@gmail.com>
2020-09-21 13:45:32 -0300

    states create

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,27 @@
-import React, {FormEvent, ChangeEvent } from 'react'
+import React, {FormEvent, ChangeEvent, useState } from 'react'
 import './styles.css'
 import Icon from '../../assets/icon.jpg'
 
 export const Login = () =>{
     const handleUserEntry = (event: ChangeEvent<HTMLInputElement>) =>{
         const {name, value} = event.target;
+        setUserEntry(value)
         console.log(name, value)
     }
+    const handlePassEntry = (event: ChangeEvent<HTMLInputElement>) =>{
+        const {name, value} = event.target;
+        setPassEntry(value)
+        console.log(name, value)
+    }
+    const [userEntry, setUserEntry] = useState('')
+    const [passEntry, setPassEntry] = useState('')
+
+    const authentication = (event: FormEvent) =>{
+        event.preventDefault()
+        console.log(userEntry)
+        console.log(passEntry)
+    }
+ 
     
     return(
         <div className="bgLogin">
@@ -16,8 +31,8 @@ export const Login = () =>{
                  <img id="iconPrincipal" src={Icon} alt=""/><br></br>
                 <form>
                 <input type="text" name="user" id="user" placeholder="Usuário" onChange={handleUserEntry} />
-                <input type="password" name="passw" id="passw" placeholder="Senha"/>
-                <button type="submit">ENTRAR</button>
+                <input type="password" name="passw" id="passw" placeholder="Senha" onChange={handlePassEntry}/>
+                <button type="submit" onClick={authentication}>ENTRAR</button>
                 </form>
             </div>
         </div>
