99c29a68bf7b80c1151203a0ba1034393421d988
Lucas Aoun <souolucas2009@gmail.com>
2020-09-23 13:29:37 -0300

    alter layout

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,6 +2,7 @@ import React, {FormEvent, ChangeEvent, useState } from 'react'
 import './styles.css'
 import Icon from '../../assets/icon.jpg'
 
+
 export const Login = () =>{
     const handleUserEntry = (event: ChangeEvent<HTMLInputElement>) =>{
         const {name, value} = event.target;
@@ -26,14 +27,22 @@ export const Login = () =>{
     return(
         <div className="bgLogin">
             <div className="bgImage">
+                <h1>Academy System</h1>
             </div>
             <div className="bgForm">
-                 <img id="iconPrincipal" src={Icon} alt=""/><br></br>
+                <img id="iconPrincipal" src={Icon} alt=""/><br></br>
                 <form>
-                <input type="text" name="user" id="user" placeholder="Usuário" onChange={handleUserEntry} />
-                <input type="password" name="passw" id="passw" placeholder="Senha" onChange={handlePassEntry}/>
-                <button type="submit" onClick={authentication}>ENTRAR</button>
-                </form>
+                    <div id="inputUser">
+                        <i className="fa fa-user"></i>  
+                        <input type="text" name="user" id="user" placeholder="Usuário" onChange={handleUserEntry} />
+                    </div>
+                    <div id="inputPass">
+                         <i className="fa fa-lock"></i>
+                         <input type="password" name="passw" id="passw" placeholder="Senha" onChange={handlePassEntry}/>
+                    </div>
+                    <a href="#">Forgot your password?</a>
+                    <button type="submit" onClick={authentication}><span>ENTRAR</span></button>
+                </form> 
             </div>
         </div>
     
