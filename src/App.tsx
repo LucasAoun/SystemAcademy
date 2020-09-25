73d5b5b42d57e9c0a0d237e72b7159f5320845ac
Lucas Aoun <souolucas2009@gmail.com>
2020-09-25 11:13:46 -0300

    react-router add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './App.css';
+import {Link, BrowserRouter, Route} from 'react-router-dom'
 import { Login } from './pages/Login';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <div>
-      <Login/>
+       <BrowserRouter basename="/">
+       <Route component={Login} path="/" exact/>
+       <Route component={Home} path="/home" exact/>
+      </BrowserRouter>
     </div>
   );
 }
