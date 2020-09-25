ae5b4f408504cdb002a4a815dee36a5b92f7c041
Lucas Aoun <souolucas2009@gmail.com>
2020-09-25 13:50:21 -0300

    Fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './styles.css'
 import Sidebar from '../../components/Sidebar'
 import Header from '../../components/Header'
+import Content from '../../components/Content'
 
 export const Home = () =>{
     return (
         <div className="Home">
            <Sidebar/>
            <Header/>
+           <Content/>
         </div>
+         
   )
 }
