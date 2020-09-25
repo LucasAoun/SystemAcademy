91973e67818baf05698e9c19b9ec8c769ce6ca0c
Lucas Aoun <souolucas2009@gmail.com>
2020-09-25 13:09:49 -0300

    sidebar with icon

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './styles.css'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () =>{
     return(
         <div className="bodySidebar">
-
+            <div className="linkPages">
+            <Link to=""><i className="fa fa-home"></i></Link>
+            <Link to=""><i className="fas fa-users" ></i></Link>
+            <Link to=""><i className="fas fa-user-circle"></i></Link>
+            <Link to=""><i className="fas fa-cog"></i></Link>
+            <Link to="" id="exitIcon"><i className="fas fa-sign-out-alt"></i></Link>
+            </div>
         </div>
     )
 }
