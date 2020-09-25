b7dbc70b0f0f7e26abc8d2e229dc57ec7e4306fd
Lucas Aoun <souolucas2009@gmail.com>
2020-09-25 13:49:53 -0300

    Content Area

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+const Content = () =>{
+    return(
+        <div>
+        <div className="contentArea"></div>
+        </div>
+    )
+}
+
+export default Content
